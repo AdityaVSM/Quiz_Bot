8c0169881e5af08e712e58cee468f93825825137
AdityaVSM <adityavsm.is19@bmsce.ac.in>
2021-06-26 00:14:54 +0530

    added error handling in create user page

--- src/swing/createAcc.java
+++ src/swing/createAcc.java
@@ -14,13 +14,18 @@ public class createAcc extends JFrame{
     private JButton continueButton;
     private JButton exitButton;
     private JPanel mainPanel;
+    private JLabel errorText;
+    private JPanel errorTextPanel;
     public String userName;
 
     public createAcc(){
+        errorText.setVisible(false);
+        textField1.setText("");
         setSize(700,300);
         setContentPane(mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -30,17 +35,26 @@ public class createAcc extends JFrame{
         continueButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
-                userName = textField1.getText();
-                UserModel user = new UserModel(userName); //If user already exists in file login else create acc
-//                System.out.println(user);
-                user.setMatchesPlayed(12);  //get data from file
-                user.setScore(450);         //get data from file
-                new DisplayProfile(userName,user);
+                if(textField1.getText().equals("")){
+                    errorText.setVisible(true);
+                    int delay = 5000; //milliseconds
+                    ActionListener taskPerformer = new ActionListener() {
+                        public void actionPerformed(ActionEvent evt) {
+                            errorText.setVisible(false);
+                        }
+                    };
+                    new javax.swing.Timer(delay, taskPerformer).start();
+                }else{
+                    userName = textField1.getText();
+                    UserModel user = new UserModel(userName); //If user already exists in file login else create acc
+                    user.setMatchesPlayed(12);  //get data from file
+                    user.setScore(450);         //get data from file
+                    new DisplayProfile(userName,user);
+                    dispose();
+                }
             }
         });
-        if(textField1.getText() != null){
-            userName = textField1.getText();
-        }
+
+
     }
 }
