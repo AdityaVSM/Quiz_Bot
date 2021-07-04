7d09a59e96883efe7def005be8440bc2d9fa4620
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2021-07-05 00:59:53 +0530

    added image for home page

--- src/swing/Home.java
+++ src/swing/Home.java
@@ -1,21 +1,31 @@
 package swing;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Home extends JFrame {
     private JPanel mainPanel;
-    private JLabel heading;
+    private JPanel imagePanel;
     private JButton signInButton;
+    private JLabel imageLabel;
     private JButton exitButton;
+    private BufferedImage mainQuizImage;
 
-    public Home(){
-        setSize(700,300);
+
+
+    public Home() {
+        setSize(1500,800);
         setContentPane(mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
         signInButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -23,16 +33,14 @@ public class Home extends JFrame {
                 new createAcc();
             }
         });
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
+
     }
 
     public static void main(String[] args) {
         new Home();
     }
 
+    private void createUIComponents() {
+        imageLabel = new JLabel(new ImageIcon("images/quizImage.jpg"));
+    }
 }
